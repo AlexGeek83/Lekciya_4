69ce26ca6e9c5077fdad2450ab5efdd73aa48c00
Alex <lrl.msk@yandex.ru>
2022-05-03 14:37:27 +0300

    add задачи

--- Задача2/Program.cs
+++ Задача2/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// рекурсия на примере факториала
+
+/*
+int Factorial(int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+for (int i = 0; i < 10; i++)
+{
+    System.Console.WriteLine($" {i}! = {Factorial(i)}");
+}
+
+*/
+
+//==========================================================
+// на примере Фибоначчи
+
+// f(1)
+// f(2)
+// f(n) = f(n-1) + f(n-2)
+
+int Fibonacci(int n)
+{
+    if (n==1 || n==2) return 1;  // если n=1 или n=2 то возвращаем 1
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 0; i < 10; i++)
+{
+  System.Console.WriteLine(Fibonacci(i));  
+}
+
+
+
--- Задача3/Program.cs
+++ Задача3/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// палец вверх
+
+int[,] pic = new int [20,20];
+
+void PrintImage (int[,] image)
+{
+    for (int i = 0; i < image.GetLength(0); i++)  // GetLength(0) - автоматически считывает 
+    //  и возвращает кол-во строк в массиве
+    {
+        for (int j = 0; j < image.GetLength(1); j++) // GetLength(0) - автоматически 
+        //  считывает кол-во столбцов в массиве
+        {
+         // System.Console.Write($" {matr[i, j]} ");
+        if(image[i,j] == 0 ) Console.Write($" ");
+        else Console.Write($"+");
+        }
+
+
+    }
+}
+
+PrintImage(pic);
+
