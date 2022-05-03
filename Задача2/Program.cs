ef36c8df0ac3abb353e7c0753a2bed8b05d22f0a
Alex <lrl.msk@yandex.ru>
2022-05-03 15:00:21 +0300

    add задачи правки

--- Задача2/Program.cs
+++ Задача2/Program.cs
@@ -27,7 +27,7 @@ int Fibonacci(int n)
     if (n==1 || n==2) return 1;  // если n=1 или n=2 то возвращаем 1
     else return Fibonacci(n-1) + Fibonacci(n-2);
 }
-for (int i = 0; i < 10; i++)
+for (int i = 1; i < 10; i++)
 {
   System.Console.WriteLine(Fibonacci(i));  
 }
